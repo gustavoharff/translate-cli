a80dbe822ee24f71e17c2887fbb2723559e9b295
Gustavo Harff <gustavo.harff@gmail.com>
2025-02-14 19:08:38 -0300

    refactor: Migrate to a interactive terminal

--- README.md
+++ README.md
@@ -14,40 +14,19 @@ $ mv ./dist/t ~/bin # ~/bin or any directory in your $PATH
 
 ## Usage
 
-```sh
-$ t --help
-```
+This CLI uses a interactive terminal, you can type any phrases in a single session.
 
-After installing it, run `t --help` without arguments to see list of options:
-
-```console
-Usage: t [options] <from> <to> <text>
-
-Arguments:
-  from        From language. Example: en
-  to          To language. Example: es
-  text        Text to translate. Example: Hello World
-
-Options:
-  -h, --help  display help for command
-```
-## Examples
-
-Translate text from English to Portuguese:
+Use the `/switch` command to change the languages.
 
 ```sh
-$ t en pt "Hello, world!"
+$ t
 ```
+
+![Example](./.github/example.png)
 
 ## Development
 
 ```sh
 $ bun i
-$ bun run start en pt "Hello, world\!"
-```
-
-```console
-ℹ Translating Hello, world! from en to pt
-
-✔ Olá, mundo!
+$ bun run start
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,93 @@
-import { program } from "commander";
 import translate from "translate";
 import logSymbols from "log-symbols";
 import chalk from "chalk";
+import * as readline from "readline";
+import isoConv from 'iso-language-converter'
+import { getFlagEmoji } from 'locale2emoji';
 
-program
-  .argument("<from>", "From language. Example: en")
-  .argument("<to>", "To language. Example: es")
-  .argument("<text>", "Text to translate. Example: Hello World")
-  .action(async (from, to, str) => {
-    console.log(
-      logSymbols.info,
-      `Translating ${chalk.bold.cyan(str)} from ${chalk.bold.magenta(
-        from
-      )} to ${chalk.bold.magenta(to)}`
-    );
-    console.log();
+let from = 'en'
+let to = 'pt'
 
-    const result = await translate(str, { from, to });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "➜ ",
+});
 
-    console.log(logSymbols.success, chalk.bold.green(result));
-    console.log();
-  });
+function clear() {
+  console.clear();
 
-program.parse();
+  console.log('Welcome to the translator!');
+
+  console.log();
+
+  const fromLang = isoConv(from.split('-')[0])
+  const toLang = isoConv(to.split('-')[0])
+
+  const fromEmoji = getFlagEmoji(from)
+  const toEmoji = getFlagEmoji(to)
+  
+  console.log('› Current translating from', chalk.cyan(fromLang), `(${fromEmoji})`, 'to', chalk.cyan(toLang) , `(${toEmoji}).`);
+
+  console.log();
+  console.log('Commands:');
+
+  console.log(chalk.bold('› /switch <from> <to>'.padEnd(21)), chalk.gray('│'), 'Switch the translation from and to languages.');
+  console.log(chalk.bold('› /clear /c'.padEnd(21)), chalk.gray('│'), 'Clear the screen.');
+  console.log(chalk.bold('› /exit /e'.padEnd(21)), chalk.gray('│'), 'Exit the program.');
+  
+  console.log();
+  console.log();
+}
+
+clear();
+
+rl.prompt();
+
+rl.on("line", async (line) => {
+  const str = line.trim();
+
+  if (str === "/exit" || str === "/e") {
+    rl.close();
+  }
+
+  if (str === "/clear" || str === "/c") {
+    clear();
+    rl.prompt();
+    return;
+  }
+
+  if (str.startsWith('/switch')) {
+    const [_, newFrom, newTo] = str.split(' ');
+
+    if (!newFrom || !newTo) {
+      console.log(logSymbols.error, 'Invalid syntax. Use /switch <from> <to>');
+      rl.prompt();
+      return;
+    }
+
+    from = newFrom;
+    to = newTo;
+
+    clear();
+
+    rl.prompt();
+    return;
+  }
+
+  if (!str) {
+    rl.prompt();
+    return;
+  }
+
+  const result = await translate(str, { from, to })
+
+  console.log(logSymbols.success, 'Translation:', chalk.bold.green(result));
+  console.log();
+
+  rl.prompt();
+});
+
+rl.on("close", () => {
+  process.exit(0);
+});
